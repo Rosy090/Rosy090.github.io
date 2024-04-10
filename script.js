4d1fed7673b34e8d3e2d3cd59eefe0172cc1f316
Rosy <sylvy909@gmail.com>
2024-04-10 16:21:19 -0500

    buttons1

--- script.js
+++ script.js
@@ -5,15 +5,20 @@ document.addEventListener("DOMContentLoaded", function() {
         const newInputGroup = document.createElement("div");
         newInputGroup.classList.add("input-group");
 
-        const newInput = document.createElement("input");
-        newInput.type = "number";
-        newInput.classList.add("numberInput");
+        const newInput1 = document.createElement("input");
+        newInput1.type = "number";
+        newInput1.classList.add("numberInput");
+
+        const newInput2 = document.createElement("input");
+        newInput2.type = "number";
+        newInput2.classList.add("numberInput");
 
         const newAddButton = document.createElement("button");
         newAddButton.textContent = "Add";
         newAddButton.classList.add("addButton");
 
-        newInputGroup.appendChild(newInput);
+        newInputGroup.appendChild(newInput1);
+        newInputGroup.appendChild(newInput2);
         newInputGroup.appendChild(newAddButton);
 
         inputsContainer.appendChild(newInputGroup);
